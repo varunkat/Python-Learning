0a36eec19063cd72d2bcaccf1ce096911ce8952e
varunkat <varun.sky56@gmail.com>
2020-11-08 14:00:02 -0700

    CLI input program

---
+++ input.py
@@ -0,0 +1,4 @@
+hours = input("Enter the total hours:") #input takes in string format
+rate = input("Enter the rate:")
+comp = float(hours) * float(rate) #converting string to float
+print("Final compensation:",comp)
