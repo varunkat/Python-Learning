31ff7562d810ec0d316a084ce869e2d8fc172d11
varunkat <varun.sky56@gmail.com>
2020-11-16 23:31:32 -0700

    added comments

--- file1.py
+++ file1.py
@@ -9,15 +9,16 @@ while True:
         try :
             fhandle = open(fname)
             for line in fhandle :
+                line = line.rstrip() #removes extra spaces after new line
                 count1 = count1 + 1
-                if line.startswith('From:') :
+                if line.startswith('From:') : # checks if the line starts with mentioned string
                     count = count + 1
                     print(line)
                 elif line.startswith('Received:') :
                     count2 = count2 + 1
         except :
             print("cant open file name:", fname)
-            quit()
+            quit() # exit the program
 
         print("Count for from is:", count)
         print("Count for Received is:", count2)
