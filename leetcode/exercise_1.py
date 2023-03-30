e2468a9f1cb74ea223c00eec1985347ca0492d0d
varun <varun.sky56@gmail.com>
2023-03-30 16:49:05 -0600

    added exercise_1.py

---
+++ leetcode/exercise_1.py
@@ -0,0 +1,6 @@
+str = 'this_is_data_engineering_users_list'
+lis1 = ['python@data.com', 'azure@databricks.com', 'glue@aws.com']
+lis2 = ['Anusha','Varun','shiva','Jeevan']
+
+#output = ['python','data','azure','databricks','glue','data','engineering','users','Anusha','Varun','shiva','Jeevan']
+
