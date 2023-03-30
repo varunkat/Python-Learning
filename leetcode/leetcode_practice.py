4835c4eb5dfc2acf6e2d4564ac46b65f18966d1a
varun <varun.sky56@gmail.com>
2023-03-30 16:41:57 -0600

    added exercise_1.py under python-learning/leetcode folder

---
+++ leetcode/leetcode_practice.py
@@ -0,0 +1,20 @@
+#today's data structures 1. Strings 2. Lists
+
+
+x = ['you','are','cool']
+y = ['you','are','cool','thankyou','varun','apple','mango']
+
+x = x + y
+print(x)
+
+for i in x[0:3]:
+    x.remove(i)
+
+print(x)
+
+str = 'this_is_data_engineering_users_list'
+lis1 = ['python@data.com', 'azure@databricks.com', 'glue@aws.com']
+lis2 = ['Anusha','Varun','shiva','Jeevan']
+
+output = ['python','data','azure','databricks','glue','data','engineering','users','Anusha','Varun','shiva','Jeevan']
+
